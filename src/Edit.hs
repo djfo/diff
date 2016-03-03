898ba8ce8b54ea8a96c24c4681002fc310e918d3
David Obwaller <david.obwaller@gmail.com>
2016-03-03 20:23:33 +0100

    shifted array indices

--- src/Edit.hs
+++ src/Edit.hs
@@ -25,17 +25,17 @@ editDistance cost s t = second reverse (d m n)
     d i 0 = go (i - 1) 0 Delete
     d 0 j = go 0 (j - 1) Insert
     d i j
-      | a ! (i - 1) == b ! (j - 1) = go (i - 1) (j - 1) Match
+      | a ! i == b ! j = go (i - 1) (j - 1) Match
       | otherwise = minimumBy (compare `on` fst)
                       [ go (i - 1) j       Delete
                       , go i       (j - 1) Insert
-                      , go (i - 1) (j - 1) (Replace (a ! (i - 1)) (b ! (j - 1)))
+                      , go (i - 1) (j - 1) (Replace (a ! i) (b ! j))
                       ]
 
     go i j op = let (score, script) = ds ! (i, j) in (score + cost op, op : script)
 
-    a = A.listArray (0, m) s
-    b = A.listArray (0, n) t
+    a = A.listArray (1, m) s
+    b = A.listArray (1, n) t
 
     ds = A.listArray bounds [d i j | (i, j) <- A.range bounds]
     bounds = ((0, 0), (m, n))
