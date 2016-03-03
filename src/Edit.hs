974a843c7afd5581d72eeea387fa76f0420c6c10
David Obwaller <david.obwaller@gmail.com>
2016-03-03 17:48:19 +0100

    use arrays for accessing list elements

--- src/Edit.hs
+++ src/Edit.hs
@@ -25,14 +25,17 @@ editDistance cost s t = second reverse (d m n)
     d i 0 = go (i - 1) 0 Delete
     d 0 j = go 0 (j - 1) Insert
     d i j
-      | s !! (i - 1) == t !! (j - 1) = go (i - 1) (j - 1) Match
+      | a ! (i - 1) == b ! (j - 1) = go (i - 1) (j - 1) Match
       | otherwise = minimumBy (compare `on` fst)
                       [ go (i - 1) j       Delete
                       , go i       (j - 1) Insert
-                      , go (i - 1) (j - 1) (Replace (s !! (i - 1)) (t !! (j - 1)))
+                      , go (i - 1) (j - 1) (Replace (a ! (i - 1)) (b ! (j - 1)))
                       ]
 
     go i j op = let (score, script) = ds ! (i, j) in (score + cost op, op : script)
+
+    a = A.listArray (0, m) s
+    b = A.listArray (0, n) t
 
     ds = A.listArray bounds [d i j | (i, j) <- A.range bounds]
     bounds = ((0, 0), (m, n))
